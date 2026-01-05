4fabd9b5c737fb1e77142312a48aca884dfacb4a
KandukuriAmar <2200030959@kluniversity.in>
2026-01-05 19:22:10 +0530

    key changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import { useState } from 'react'
 function App() {
   const handlePayment = async () => {
     const backurl = import.meta.env.VITE_BACKEND_URL
-    const razorpaykey = import.meta.env.RAZORPAY_KEY_ID
-    const data = await axios.post("${backurl}/create-order", {amount});
+    const razorpaykey = import.meta.env.VITE_RAZORPAY_KEY_ID
+    const data = await axios.post(`${backurl}/create-order`, {amount});
   
     const options = {
       key: razorpaykey,
@@ -20,31 +20,48 @@ function App() {
       },
       handler: async(response) => {
         try {
-          await axios.post("${backurl}/validate-payment", response);
-          alert("payment successful");
+          await axios.post(`${backurl}/validate-payment`, response);
+          setFlag(true);
+          setTextColor("green");
+          setText("payment successful");
+          setTimeout(() => {
+            setFlag(false);
+          }, 3000);
         } catch(err) {
+          setFlag(true);
           console.error(err);
-          alert("Payment verification failed");
+          setTextColor("red");
+          setText("Payment verification failed");
+          setTimeout(() => {
+            setFlag(false);
+          }, 3000);
         }
-      },
-      theme: {
-        color: "#3399cc"
       }
     };
 
     const rzp = new window.Razorpay(options);
     rzp.open();
 
-    rzp.on("payment.failed", (res) => {
+    rzp.on("payment.failed", () => {
       alert("payment failed");
     });
   }
   const [amount, setAmount] = useState(0);
+  const [flag, setFlag] = useState(false);
+  const [text, setText] = useState("");
+  const [textColor, setTextColor] = useState("");
+  
   return (
     <>
-      <h1>QR Code Payment</h1>
-      <input type='text' placeholder='Enter amount' value={amount} onChange={(e) => setAmount(e.target.value)} />
-      <button onClick={handlePayment}>payOut</button>
+      {flag && <h1 style={{color: textColor}}>{text}</h1>}
+
+      {!flag && 
+      <>
+        <h1>QR Code Payment</h1>
+        <input type='text' placeholder='Enter amount' value={amount} onChange={(e) => setAmount(e.target.value)} />
+        <button onClick={handlePayment}>payOut</button>
+      </>
+      }
     </>
   );
 }
